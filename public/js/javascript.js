1931e3ebf6a8c7f8eb0190fe748d65be7218f690
Gabriel Perry <gperry22@gmail.com>
2019-06-19 15:12:09 -0400

    stopping point

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,7 +1,6 @@
 console.log("App Running");
 
-
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyDL7X67L0Gcil8zzP6huH9pyVFXpmMDQwE",
     authDomain: "chatty-2206b.firebaseapp.com",
     databaseURL: "https://chatty-2206b.firebaseio.com",
@@ -14,21 +13,53 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 var provider = new firebase.auth.GoogleAuthProvider();
+var userToken = notLoggedInMessage()
+var dbListOfUserNames = [];
+
+
+isUserLoggedIn()
+
+
+function isUserLoggedIn() {
+var userNotLoggedIn = notLoggedInMessage()
+if(userToken === userNotLoggedIn) {
+      $("#chatBox1").css("display", "none")
+      console.log(userToken);
+      showSignIn()
+      hideSignOut()
+   }
+   else if (userToken) {
+      $("#chatBox1").css("display", "")
+      console.log(userToken);
+      hideSignIn()
+      showSignOut()
+   }
+}
+
+//************************FIREBASE GOOGLE AUTH************************/
 
 function googleSignIn() {
    firebase.auth()
-
    .signInWithPopup(provider).then(function(result) {
       var token = result.credential.accessToken;
-      var user = result.user;
-
       console.log(token)
+
+      var user = result.user;
       console.log(user)
-      $("#signIn").css("display", "none");
+
+      var userToSave = {
+         name: user.displayName,
+         email: user.email,
+         photo: user.photoURL,
+         token: token
+      }
+      console.log(userToSave);
+
+      isUserSignedUpAlready(userToSave, token)
+
    }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
-
       console.log(error.code)
       console.log(error.message)
    });
@@ -36,11 +67,153 @@ function googleSignIn() {
 
 function googleSignOut() {
    firebase.auth().signOut()
-
    .then(function() {
       console.log('Signout Successful')
-      $("#signIn").css("display", "");
+      clearSessionStorageAndToken()
    }, function(error) {
       console.log('Signout Failed')
    });
 }
+
+//*******************CHECKING TO SEE IF USER IS ALREADY SIGNED UP************************/
+
+function isUserSignedUpAlready(userToSave, token) {
+   database.ref("/users").on("value", function (snapshot) {
+      console.log(snapshot.val());
+      if (snapshot.val()) {
+         var dbSnapShot = snapshot.val()
+
+         var dbListOfUserEmails = [];
+         var entries = Object.entries(dbSnapShot)
+         console.log(entries);
+
+         for (var i = 0; i < entries.length; i++) {
+            dbListOfUserEmails.push(entries[i][1].email)
+            dbListOfUserEmails.push(entries[i][1].name)
+         }
+         console.log(dbListOfUserEmails);
+
+
+         if (dbListOfUserEmails.indexOf(userToSave.email) === -1) {
+            console.log("Saving new User");
+            saveUser(userToSave)
+            saveToken(userToSave)
+            isUserLoggedIn()
+         }
+         else {
+            console.log("User already Signed Up");
+            saveToken(userToSave)
+            isUserLoggedIn()
+         }
+      }
+      else {
+            saveUser(userToSave)
+            saveToken(userToSave)
+            isUserLoggedIn()
+      }
+   })
+}
+
+
+function saveUser(userToSave) {
+   database.ref("/users").push(userToSave)
+   isUserSignedUpAlready()
+}
+
+function saveToken(userToSave) {
+   sessionStorage.setItem("user", userToSave.name)
+   sessionStorage.setItem("userToken", userToSave.token)
+   sessionStorage.setItem("userPhoto", userToSave.photo)
+   userToken = sessionStorage.getItem("userToken")
+}
+
+//************************Hide/Show SignIn and SignOut Buttons************************/
+function hideSignIn() {
+   $("#signIn").css("display", " none");
+}
+
+function hideSignOut() {
+   $("#signOut").css("display", "none");
+}
+
+function showSignOut() {
+   $("#signOut").css("display", "");
+}
+
+function showSignIn() {
+   $("#signIn").css("display", "");
+}
+
+function clearSessionStorageAndToken() {
+   sessionStorage.clear();
+   userToken = notLoggedInMessage()
+   isUserLoggedIn()
+}
+
+function notLoggedInMessage(){
+   return "Not Logged In"
+}
+//************************************************/
+
+
+
+
+
+//************************Get and pushing Chats to DOM************************/
+$("#submit").click(function (e) {
+   e.preventDefault();
+   var userChatMessage = $("#userMessage").val().trim()
+   console.log(userChatMessage);
+   if (userChatMessage) {
+   saveUserMessageToFirebase(userChatMessage)
+   $("#userMessage").val(" ")
+
+   }
+   else {
+      alert("Please enter a chat message")
+   }
+
+});
+
+function saveUserMessageToFirebase(message) {
+   var photoLink = sessionStorage.getItem("userPhoto")
+   database.ref("/messages").push({
+      chat: message,
+      photo:photoLink
+   })
+}
+
+
+//************************Get and pushing EACH NEW Chat to DOM************************/
+
+   database.ref("/messages").on("child_added", function (childSnapShot, prevChildKey) {
+      console.log(prevChildKey);
+      console.log(childSnapShot.val());
+      var message = childSnapShot.val().chat
+      console.log(message);
+      var photoURL = childSnapShot.val().photo
+      console.log(photoURL);
+
+         var chatDiv = $("<div class='row'>")
+         var photoCol = $("<div class='col-md-2'>")
+         chatDiv.addClass("divSpace mborder")
+
+         var messageCol = $("<div class='col-md-9'>")
+      messageCol.text(message)
+      messageCol.addClass("messBorder")
+
+
+         var userPhoto = $("<img>")
+         userPhoto.attr("src", photoURL);
+         userPhoto.attr("alt", "User Photo");
+         userPhoto.addClass("userPhotoSize");
+         photoCol.append(userPhoto)
+
+         chatDiv.append(photoCol, messageCol)
+
+
+         $("#allChatTexts").append(chatDiv)
+
+
+   })
+
